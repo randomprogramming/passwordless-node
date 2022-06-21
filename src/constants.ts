4e7d7e11d7754f72a44548edbf6b2f22964b2066
randomprogramming <dgoluban38@gmail.com>
2022-06-21 12:14:12 +0200

    update public key for prod

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,10 @@
 export const PASSWORDLESS_SERVER_BASE_URL = "https://aws.unpass.me/api";
 export const AUTH_PUBLIC_KEY =
   "-----BEGIN RSA PUBLIC KEY-----\n" +
-  "MIIBCgKCAQEAnP8QVvN4kvTViB4NuT0C8armrzZYTQMB4hrPTbgp5+v/7yAblCIw\n" +
-  "6DGFSLYse7l7wZSK/EFt8zChRfhcgezcuxlXHYIYRV4Js8WrkSWafuc0z4F+aoJl\n" +
-  "8rlynHOdAkYtWfwLFcKGPhKgupWjHDxyga+BotDszNFAHzLlHVf4vu6FirA91TY0\n" +
-  "xQGD90d3QRXydUT2mMZfMqgRZ/2c6ybn7OPRBWgxgXdIb4SCaQRiDN3eccO/OwGJ\n" +
-  "GmjTOuv6Jzl4GT8k/ijdbKBvBGqqQ3A6MH8B6isTAY1K0LoMUF1cU7xKDgg278na\n" +
-  "bjAXblOsb/gu4dUytfTrCY72EuUvbJMuWwIDAQAB\n" +
+  "MIIBCgKCAQEAxFeUEq/Dgpqu/CqSb6v68xprPqfrj1k4cobz4nY1X7B0Ov+7KdMj\n" +
+  "9YybgCr2peGKsvnfHvDmVPqDLsN88/G0zMrMdZbbI7SLhojABhq/asNfvb9RKf8Q\n" +
+  "u6XRQx23hKtZgggUjw951/VuRPLBRs4yTAcHflNWehx29ZuEeyEIcHXYj/n8bJGu\n" +
+  "3savFagFEJjle8awJ3WwCPwQJGMzOWR+SJssRO7XgAadBUy5ymz56KkmqDAqZhRD\n" +
+  "n/kcTyVBBjOCAr4h7ET7DFBI3MIEvmOM9wRgMoAF1ejAym/nkwv7Rvy2HmOxipwZ\n" +
+  "7BmurTbTBL3jwpwRrUUCt3+x0yThgmUMbwIDAQAB\n" +
   "-----END RSA PUBLIC KEY-----";
